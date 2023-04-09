8e2c8abf24a16dc6a4db57dec12f2622bc12b6af
Kunal Singh <kunalsin9h@gmail.com>
2023-04-09 11:47:11 +0530

    add make.sh

---
+++ make.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/bash
+g++ -std=c++17 -O2 -Wall -Wextra -Wshadow -DLOCAL -fsanitize=undefined -fsanitize=address -Wformat=2 -Wfloat-equal -Wconversion -Wcast-qual -Wcast-align -Wno-sign-conversion -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fno-sanitize-recover=all -fstack-protector -D_FORTIFY_SOURCE=2 -o $1.out $1.cpp
+
